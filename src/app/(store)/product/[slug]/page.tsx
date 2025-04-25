7f7a184fe54460987cadc9acb8826626081b2db1
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-04-26 00:11:22 +0530

    deploying error

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,6 +1,9 @@
+import { imageUrl } from "@/lib/imageUrl";
+import Image from "next/image";
 import { getProductBySlug } from "@/sanity/lib/Products/getProductBySlug";
 import { notFound } from "next/navigation";
 import React from "react";
+import { PortableText } from "next-sanity";
 
 async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
@@ -10,7 +13,43 @@ async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
   if (!product) {
     return notFound();
   }
-  return <div>ProductPage</div>;
+
+  const isOutOfStock = product.stock != null && product.stock <= 0;
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div
+          className={`relative aspect-square overflow-hidden shadow-lg rounded-lg ${isOutOfStock ? "opacity-50" : ""}`}
+        >
+          {product.image && (
+            <Image
+              fill
+              className="object-contain transition-transform duration-300 hover:scale-105"
+              src={imageUrl(product.image).url()}
+              alt={product.name || "Product Image"}
+            />
+          )}
+          {isOutOfStock && (
+            <div className="absolute inset-0 bg-black flex items-center justify-center bg-opacity-50">
+              <span className="text-white font-bold text-lg">Out of Stock</span>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col justify-between">
+          <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+          <div className="text-xl font-semibold mb-4">
+            Rs {product.price?.toFixed(2)}
+          </div>
+          <div className="prose max-w-none mb-6">
+            {Array.isArray(product.description) && (
+              <PortableText value={product.description} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import { searchProductsByName } from "@/sanity/lib/Products/searchProductsByName";
 import ProductGrid from "@/components/ProductGrid";
 
-async function SearchPage({
-  searchParams,
-}: {
-  searchParams: { params: Promise<{ query: string }> };
-}) {
-  const { query } = await searchParams.params;
+type SearchPageProps = {
+  searchParams?: {
+    query?: string;
+  };
+};
+
+async function SearchPage({ searchParams }: SearchPageProps) {
+  const query = searchParams?.query ?? "";
 
   const products = await searchProductsByName(query);
 
   if (!products.length) {
     return (
-      <div className="flex flex-col items-center justify-top min-h-screem bg-gray-100 p-4">
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
           <h1 className="text-3xl font-bold mb-6 text-center">
             No products found for: {query}
@@ -25,8 +27,9 @@ async function SearchPage({
       </div>
     );
   }
+
   return (
-    <div className="flex flex-col items-center justify-top min-h-screem bg-gray-100 p-4">
+    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
         <h1 className="text-3xl font-bold mb-6 text-center">
           Search Results for: {query}
